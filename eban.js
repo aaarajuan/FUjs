d752024e1e7d0a43c26c9ead3204b8b3a2f584ef
Al Arashad Ahmed Rajuan (Ongkur) <71528293+aaarajuan@users.noreply.github.com>
2022-02-14 22:42:06 +0600

    Create eban.js

---
+++ eban.js
@@ -0,0 +1,18 @@
+
+    //add getInputValue101();
+function getInputValue101() {
+    var authCode = 'Bb3!k';
+  // add the output element and get its value
+  var valuepr = "<body style='background-color:black;'><center style='color: green;'>Welcome User! You Are Now Authorised Your Download URL is:<p style='color: yellow;'><--All Of Are Us Dead (Season 01)--></p><p style='color: red;'><--Video Resolutions Available For Download: 720p, 1080p--></p><br/><br/><p style='color: red;'><-- Watch Links --></p><a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/2p8wr5ja'>360p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/2p8wr5ja'>480p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/2p8wr5ja'>720p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/2p8wr5ja'>1080p</a><br/><br/><p style='color: red;'><-- Direct Download Links --></p><a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>360p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>480p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>720p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>1080p</a><br/><br/><p style='color: red;'><-- Torrent Download --></p><a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>360p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>480p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>720p</a> | <a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>1080p</a><br/><br/><p style='color: red;'><--Download Subtitles--></p><a  target='_blank' style='font-size: 14px; color: skyblue;' href='https://tinyurl.com/ycknaa4p'>- - -</a></center></body>'";
+  var value = "<body style='background-color: #000;'><center style='color: skyblue;'>xXx Uh O!! You Are Not a Authorized Person. xXx</center></body>";
+  // Selecting the input element and get its value
+  // add myInput11
+  var inputVal = document.getElementById("myInput101").value;
+  // Displaying the value
+  // add authorize match code
+  if (inputVal == authCode) {
+    document.write(valuepr);
+  } else {
+    document.write(value);
+  }
+}
